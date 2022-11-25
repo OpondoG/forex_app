d47f0aafcba3675832e887c9940ee62379e178c2
OpondoG <okonjogilbert2@gmail.com>
2022-11-25 18:11:54 +0300

    Modify Styling the header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-  <Footer>
+  <section className="footer">
     <a href="https://financialmodelingprep.com/developer/docs/" target="_blank" rel="noreferrer">Data provided by Financial Modeling Prep</a>
     <a href="https://github.com/OpondoG" target="_blank" rel="noreferrer">Developed by Gilbert Okonjo</a>
-  </Footer>;
+  </section>;
 };
 export default Footer;
